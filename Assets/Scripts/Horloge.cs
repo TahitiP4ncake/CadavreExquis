7336a123affc95626c72f6eb370eb2ebf67e2ade
Dorian Popcorn <TahitiP4ncake@users.noreply.github.com>
2019-08-12 16:38:22 +0200

    01_Dorian
    horloge synchronysée avec l'heure locale
    chaque aiguille tourne sur un axe local différent (secondes x, minutes y, heures z)

---
+++ Assets/Scripts/Horloge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Horloge : MonoBehaviour
+{
+    public Transform hourObject;
+    public Transform minuteObject;
+    public Transform secondObject;
+    
+    private void Update()
+    {
+        DateTime current = DateTime.Now;
+        secondObject.transform.localEulerAngles = new Vector3(360/60*current.Second,0,0);
+        minuteObject.transform.localEulerAngles = new Vector3(0,360/60*current.Minute,0);
+        hourObject.transform.localEulerAngles = new Vector3(0,0,360/60*(current.Hour%12));
+    }
+
+}
